0118290919c734e34243965c998e124692b64226
devkudasov <dev.kudasov@gmail.com>
2019-01-16 15:36:15 +0300

    Complete watcher

--- watcher/index.js
+++ watcher/index.js
@@ -1,13 +1,15 @@
 const path = require('path');
 const fs = require('fs');
 const Watcher = require('./Watcher');
-const renameDir = path.join(__dirname, '/rename');
 
-const watcher = new Watcher(__dirname, renameDir);
+const fromDir = path.join(__dirname, '/from');
+const toDir = path.join(__dirname, '/to');
+
+const watcher = new Watcher(fromDir, toDir);
 
 watcher.on('process', file => {
-  const watchFile = `${watchDir}/${file}`;
-  const processFile = `${renameDir}/${file.toLowerCase()}`;
+  const watchFile = `${fromDir}/${file}`;
+  const processFile = `${toDir}/${file.toLowerCase()}`;
   
   fs.rename(watchFile, processFile, err => {
     if (err) throw err;
